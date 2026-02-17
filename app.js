832a47bb315c03f8418a4ffa0ae13a253443777f
Jo Greenlee <sweettroubles29@gmail.com>
2026-02-16 21:03:26 -0600

    Added to Js

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const firstName = "Jodie";
+let score = 40;
+const isActive = true;
+const tags = ["js", "week 3"];
+
+console.log(firstName);
+console.log(score);
+console.log(isActive);
+console.log(tags);
+
+const user = {
+  id: 1,
+  favoriteColor: "Yellow",
+};
+console.log(user.favoriteColor);
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(2, 3));
